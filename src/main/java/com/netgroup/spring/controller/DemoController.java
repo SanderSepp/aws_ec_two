90a5ea0a7d56be1aba1643f9882b89a44ec5d968
sandersepp <sander.sepp@netgroup.com>
2022-10-03 12:13:51 +0300

    Initial commit

---
+++ src/main/java/com/netgroup/spring/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.netgroup.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
